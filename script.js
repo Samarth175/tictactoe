ba6ceb8ae8b51c45988d5743e2cf712019ea1ca1
Samarth Gupta <samarth1751997@gmail.com>
2019-11-01 19:38:04 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var currentPlayer = 0;
+var currentStatus = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+var clickSound = new Audio("/audio/click.wav");
+var overSound = new Audio("/audio/over.wav");
+
+function itemClicked(item) {
+    const playerValue = (currentPlayer) ?"O":"X";
+    const itemId = parseInt(item.id);
+    if(currentStatus[itemId]==-1){
+        item.innerHTML = playerValue;
+        currentStatus[itemId] = currentPlayer;
+        clickSound.play();
+    }else{
+        return alert("Invaid Move");
+    }
+    if(hasWon()){
+        overSound.play();
+        alert("Player " + playerValue + " has won the game!");
+        resetGame();
+        return;
+    }
+    currentPlayer = !currentPlayer;
+}
+
+function hasWon(){
+    if(currentStatus[0]!=-1 && currentStatus[0]==currentStatus[1] && currentStatus[0]==currentStatus[2])
+        return true;
+    else if(currentStatus[3]!=-1 && currentStatus[3]==currentStatus[4] && currentStatus[3]==currentStatus[5])
+        return true;
+    else if(currentStatus[6]!=-1 && currentStatus[6]==currentStatus[7] && currentStatus[6]==currentStatus[8])
+        return true;
+    else if(currentStatus[0]!=-1 && currentStatus[0]==currentStatus[3] && currentStatus[0]==currentStatus[6])
+        return true;
+    else if(currentStatus[1]!=-1 && currentStatus[1]==currentStatus[4] && currentStatus[1]==currentStatus[7])
+        return true;
+    else if(currentStatus[2]!=-1 && currentStatus[2]==currentStatus[5] && currentStatus[2]==currentStatus[8])
+        return true;
+    else if(currentStatus[0]!=-1 && currentStatus[0]==currentStatus[4] && currentStatus[0]==currentStatus[8])
+        return true;
+    else if(currentStatus[2]!=-1 && currentStatus[2]==currentStatus[4] && currentStatus[2]==currentStatus[6])
+        return true;
+    return false;
+}
+
+function resetGame(){
+    currentPlayer = 0;
+    currentStatus = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+    const items = document.querySelectorAll("td");
+    items.forEach(item => {
+        item.innerHTML = "<img src='icon.png' alt='--'>";
+    });
+}
